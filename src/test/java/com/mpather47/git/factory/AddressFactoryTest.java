2291aa784b073c382cc24bfccd88ff265713eb8c
Marcell <mpather47@gmail.com>
2020-07-03 16:37:47 +0200

    Changed Test case again

--- src/test/java/com/mpather47/git/factory/AddressFactoryTest.java
+++ src/test/java/com/mpather47/git/factory/AddressFactoryTest.java
@@ -8,17 +8,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AddressFactoryTest {
-    // Tests if the method returns a null value
+    // Tests if the method returns a value
     @Test
     public void addAddress() {
         Address address = AddressFactory.addAddress("Test case");
-        Assert.assertEquals(null, address.getAddress());
+        Assert.assertNotNull(address.getAddress());
     }
-    // Tests if the method returns a null value
+    // Tests if the method returns a value
     @Test
     public void addPostalCode() {
             Address address = AddressFactory.addPostalCode("Test case");
-            Assert.assertEquals(null, address.getPostcode());
+            Assert.assertNotNull(address.getPostcode());
     }
 
 
