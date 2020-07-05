c953a63cd0dcb30e88a40c863dc717cdbfe9f804
Goodman Nkosekhaya Booi <36563189+GoodmanB@users.noreply.github.com>
2020-07-05 13:13:42 +0200

    DepartmentFactory and EmployeeFactory

---
+++ src/main/java/com/mpather47/git/factory/DepartmentFactory.java
@@ -0,0 +1,14 @@
+package com.mpather47.git.factory;
+
+import com.mpather47.git.entity.Department;
+
+public class DepartmentFactory {
+    public static Department createDepartment(int departmentID, String ward, String role){
+        Department department = new Department.DepartmentBuilder().
+                setDepartmentId(departmentID).
+                setWard(ward).
+                setRole(role).build();
+
+        return department;
+    }
+}
