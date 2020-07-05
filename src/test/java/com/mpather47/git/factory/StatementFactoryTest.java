8bb9d97f9991d2cd19f055e8a3af9c1fbbffbc24
XodusDR <sadokun113@gmail.com>
2020-07-05 22:52:40 +0200

    Updating

--- src/test/java/com/mpather47/git/factory/StatementFactoryTest.java
+++ src/test/java/com/mpather47/git/factory/StatementFactoryTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import static org.junit.Assert.*;
 
 public class StatementFactoryTest {
-
+// added import for Assert
     @org.junit.Test
     public void createStatement() {
         Statement statement = StatementFactory.createStatement(7);
