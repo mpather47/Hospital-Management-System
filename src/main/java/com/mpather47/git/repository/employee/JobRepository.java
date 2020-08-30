a13cca144b41e05753e86dc88ae951b9094b2fcc
mpather47 <mpather47@gmail.com>
2020-08-31 00:18:45 +0200

    final

--- src/main/java/com/mpather47/git/repository/employee/JobRepository.java
+++ src/main/java/com/mpather47/git/repository/employee/JobRepository.java
@@ -2,10 +2,11 @@ package com.mpather47.git.repository.employee;
 
 import com.mpather47.git.entity.employee.Job;
 import com.mpather47.git.entity.employee.Position;
+import com.mpather47.git.repository.IRepository;
 
 import java.util.Set;
 
-public interface JobRepository extends Repository <Job, String>{
+public interface JobRepository extends IRepository<Job, String>{
 
 
     Set<Job> getAll();
