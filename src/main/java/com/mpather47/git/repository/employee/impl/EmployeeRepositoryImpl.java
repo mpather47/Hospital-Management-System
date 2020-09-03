526a6619a74a869065a5f3de12ce46f03b7ebc40
mpather47 <mpather47@gmail.com>
2020-09-03 19:54:32 +0200

    Resolved COnflicts

--- src/main/java/com/mpather47/git/repository/employee/impl/EmployeeRepositoryImpl.java
+++ src/main/java/com/mpather47/git/repository/employee/impl/EmployeeRepositoryImpl.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.mpather47.git.repository.employee.impl;
 
 import com.mpather47.git.entity.employee.Employee;
@@ -62,68 +62,3 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
         return employees;
     }
 }
-=======
-package com.mpather47.git.repository.employee.impl;
-
-import com.mpather47.git.entity.employee.Employee;
-import com.mpather47.git.repository.employee.EmployeeRepository;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class EmployeeRepositoryImpl implements EmployeeRepository {
-
-    private Set<Employee> employees;
-    private static EmployeeRepository employeeRepository = null;
-
-    public EmployeeRepositoryImpl() {
-        this.employees = new HashSet<>();
-    }
-
-    public static EmployeeRepository getEmployeeRepository(){
-        if (employeeRepository == null)
-            employeeRepository = new EmployeeRepositoryImpl();
-        return employeeRepository;
-    }
-
-    @Override
-    public Employee create(Employee employee){
-        employees.add(employee);
-        return employee;
-    }
-
-    @Override
-    public Employee read(final String employeeId){
-        return employees.stream().filter(employee -> employee.getEmployeeID().equals(employeeId)).
-                findAny().orElse(null);
-    }
-
-
-    @Override
-    public Employee update(Employee employee){
-        Employee oldEmployee = read(employee.getEmployeeID());
-        if (oldEmployee != null){
-            employees.remove(oldEmployee);
-            employees.add(employee);
-        }
-
-        return employee;
-    }
-
-    @Override
-    public boolean delete(String employeeId){
-        boolean deleted = false;
-        Employee oldEmployee = read(employeeId);
-        if (oldEmployee != null) {
-            employees.remove(oldEmployee);
-            deleted = true;
-        }
-        return deleted;
-    }
-
-    @Override
-    public Set<Employee> getAll() {
-        return employees;
-    }
-}
->>>>>>> d8ea9913b6267ce59dcd4d87a709e8bbd7676bfb
--- src/test/java/com/mpather47/git/repository/hospital/impl/DepartmentRepositoryImplTest.java
+++ src/test/java/com/mpather47/git/repository/hospital/impl/DepartmentRepositoryImplTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.mpather47.git.repository.hospital.impl;
 
 import com.mpather47.git.entity.hospital.Department;
@@ -51,57 +51,3 @@ public class DepartmentRepositoryImplTest {
         assertEquals(1, departments.size());
     }
 }
-=======
-package com.mpather47.git.repository.hospital.impl;
-
-import com.mpather47.git.entity.hospital.Department;
-import com.mpather47.git.factory.hospital.DepartmentFactory;
-import com.mpather47.git.repository.hospital.DepartmentRepository;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
-
-import java.util.Set;
-
-import static org.junit.Assert.*;
-
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class DepartmentRepositoryImplTest {
-
-
-    private static DepartmentRepository repository = DepartmentRepositoryImpl.getDepartmentRepository();
-    private static Department department = DepartmentFactory.createDepartment(21,"The ward","The role");
-
-    @Test
-    public void a_create() {
-        Department created = repository.create(department);
-        assertEquals(department.getDepartmentId(), created.getDepartmentId());
-    }
-
-    @Test
-    public void b_read() {
-        Department read = repository.read(department.getDepartmentId());
-        assertEquals(department.getDepartmentId(), read.getDepartmentId());
-    }
-
-    @Test
-    public void c_update() {
-        Department updated = new Department.DepartmentBuilder().copy(department).
-                setRole("Bones").build();
-
-        updated = repository.update(updated);
-        assertEquals(department.getDepartmentId(), updated.getDepartmentId());
-    }
-
-    @Test
-    public void e_delete() {
-        repository.delete(department.getDepartmentId());
-    }
-
-    @Test
-    public void d_getAll() {
-        Set<Department> departments = repository.getAll();
-        assertEquals(1, departments.size());
-    }
-}
->>>>>>> d8ea9913b6267ce59dcd4d87a709e8bbd7676bfb
