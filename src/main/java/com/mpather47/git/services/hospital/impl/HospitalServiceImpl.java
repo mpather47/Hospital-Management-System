675f7759d6219a8a0e21b69ca0130ec20a1dbe15
GooseRooster <cameron.mostert@gmail.com>
2020-09-04 20:10:16 +0200

    Created impl and test cases for Room, Equipment and Hospital (services)

---
+++ src/main/java/com/mpather47/git/services/hospital/impl/HospitalServiceImpl.java
@@ -0,0 +1,51 @@
+package com.mpather47.git.services.hospital.impl;
+
+import com.mpather47.git.entity.hospital.Hospital;
+import com.mpather47.git.repository.hospital.HospitalRepository;
+import com.mpather47.git.repository.hospital.impl.HospitalRepositoryImpl;
+import com.mpather47.git.services.hospital.HospitalService;
+
+import java.util.Set;
+
+public class HospitalServiceImpl implements HospitalService {
+    private static HospitalService service=null;
+    private HospitalRepository repository;
+
+    private HospitalServiceImpl() {
+        this.repository = HospitalRepositoryImpl.getRepository();
+    }
+
+    public static HospitalService getService(){
+        if(service==null) service = new HospitalServiceImpl();
+        return service;
+    }
+
+
+    @Override
+    public Set getAll() {
+        return this.repository.getAll();
+    }
+
+    @Override
+    public Hospital create(Hospital hospital) {
+        return this.repository.create(hospital);
+    }
+
+    @Override
+    public Hospital read(String s) {
+
+        return this.repository.read(s);
+    }
+
+    @Override
+    public Hospital update(Hospital hospital) {
+
+        return this.repository.update(hospital);
+    }
+
+    @Override
+    public boolean delete(String s) {
+
+        return this.repository.delete(s);
+    }
+}
---
+++ src/test/java/com/mpather47/git/services/hospital/impl/HospitalServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.mpather47.git.services.hospital.impl;
+
+import com.mpather47.git.entity.hospital.Hospital;
+import com.mpather47.git.factory.hospital.HospitalFactory;
+import com.mpather47.git.services.hospital.HospitalService;
+import com.mpather47.git.utility.Helper;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class HospitalServiceImplTest {
+    private static HospitalService service = HospitalServiceImpl.getService();
+    private static Helper help = new Helper();
+    private static Hospital hospital = HospitalFactory.createHospital(help.generateId(), "St Mary", 2456);
+    @Test
+    public void d_testGetAll() {
+        Set<Hospital> hospital = service.getAll();
+        assertEquals(1,hospital.size());
+        System.out.println("All hospitals: " + hospital);
+    }
+    @Test
+    public void a_testCreate() {
+        Hospital created = service.create(hospital);
+        Assert.assertEquals(hospital.getHospitalId(), created.getHospitalId());
+        System.out.println("Created:" + created);
+    }
+    @Test
+    public void b_testRead() {
+        Hospital read = service.read(hospital.getHospitalId());
+        System.out.println("Read:" + read);
+    }
+    @Test
+    public void c_testUpdate() {
+        Hospital updated = new Hospital.Builder().copy(hospital).setHospitalName("St Mary").build();
+        updated = service.update(updated);
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    public void e_testDelete() {
+        boolean deleted = service.delete(hospital.getHospitalId());
+        Assert.assertTrue(deleted);
+    }
+}
+
