8d1fb8bb429a764197e63a69b7c53ab10c55bae3
Luyanda2020 <mthura2214@gmail.com>
2020-10-21 12:29:14 +0200

    reverted

--- src/test/java/com/mpather47/git/controller/account/InvoiceControllerTest.java
+++ src/test/java/com/mpather47/git/controller/account/InvoiceControllerTest.java
@@ -1,11 +1,10 @@
-package com.mpather47.git.controller;
-
+package com.mpather47.git.controller.account;
 
 import com.mpather47.git.entity.account.Invoice;
 import com.mpather47.git.factory.account.AccountFactory;
 import com.mpather47.git.factory.account.InvoiceFactory;
 import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +16,23 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-class InvoiceControllerTest {
+public class InvoiceControllerTest {
 
-    private static Invoice invoice = InvoiceFactory.createInvoice("27/07/2023","Coka Cola", AccountFactory.createAccount(555.00,"Payroll"));
+    private Invoice invoice = InvoiceFactory.createInvoice("27/07/2023","Coka Cola", AccountFactory.createAccount(555.00,"Payroll"));
     @Autowired
     private TestRestTemplate restTemplate;
     private String baseURL = "http://localhost:8080/invoice/";
 
-
     @Test
-    void a_create() {
+    public void a_create() {
+
         String url = baseURL + "create/";
         System.out.println("URL: " + url);
         System.out.println("Post data: " + invoice);
@@ -43,40 +43,32 @@ class InvoiceControllerTest {
         System.out.println("Saved data: " + invoice);
         assertEquals(invoice.getInvoiceNum(),postResponse.getBody().getInvoiceNum());
 
-
     }
 
     @Test
-    void b_read() {
+    public void b_read() {
         String url = baseURL + "read/" + invoice.getInvoiceNum();
         System.out.println("URL: " + url);
         ResponseEntity<Invoice> response = restTemplate.getForEntity(url,Invoice.class);
-        assertEquals(invoice.getInvoiceNum(),response.getBody().getInvoiceNum());
-
-
+        invoice = response.getBody();
+        assertEquals(invoice.getInvoiceNum(), response.getBody().getInvoiceNum());
 
     }
 
     @Test
-    void c_update() {
+    public void c_update() {
         Invoice updated = new Invoice.Builder().copy(invoice).setInvoiceDate("07/01/2021").setDescription("LG Refrigerant").build();
         String url = baseURL + "update/";
         System.out.println("Post data:" + updated);
         ResponseEntity<Invoice> response = restTemplate.postForEntity(url,updated, Invoice.class);
-        System.out.println(response);
+        invoice = response.getBody();
+        System.out.println(invoice);
         assertEquals(invoice.getInvoiceNum(),response.getBody().getInvoiceNum());
 
     }
 
     @Test
-    void e_delete() {
-        String url = baseURL + "delete/" + invoice.getInvoiceNum();
-        System.out.println("URL: " + url);
-        restTemplate.delete(url);
-    }
-
-    @Test
-    void d_getAll() {
+    public void d_getAll() {
         String url = baseURL + "all/";
         HttpHeaders headers= new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null,headers);
@@ -85,5 +77,10 @@ class InvoiceControllerTest {
         System.out.println(response.getBody());
     }
 
-
+    @Test
+    public void e_delete() {
+        String url = baseURL + "delete/" + invoice.getInvoiceNum();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
+    }
 }
--- src/test/java/com/mpather47/git/controller/account/StatementControllerTest.java
+++ src/test/java/com/mpather47/git/controller/account/StatementControllerTest.java
@@ -1,10 +1,10 @@
-package com.mpather47.git.controller;
+package com.mpather47.git.controller.account;
 
 import com.mpather47.git.entity.account.Statement;
 import com.mpather47.git.factory.account.AccountFactory;
 import com.mpather47.git.factory.account.StatementFactory;
 import org.junit.FixMethodOrder;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +16,20 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-class StatementControllerTest {
+public class StatementControllerTest {
 
-    private static Statement statement = StatementFactory.createStatement("10/10/2010",100000000.00, AccountFactory.createAccount(7656788.98,"Sale"));
+    private Statement statement = StatementFactory.createStatement("10/10/2010",80000.00, AccountFactory.createAccount(7656788.98,"Sale"));
     @Autowired
     private TestRestTemplate restTemplate;
     private String baseURL = "http://localhost:8080/statement/";
 
     @Test
-    void a_create() {
+    public void a_create() {
         String url = baseURL + "create/";
         System.out.println("URL: " + url);
         System.out.println("Post data: " + statement);
@@ -41,48 +40,45 @@ class StatementControllerTest {
         System.out.println("Saved data: " + statement);
         assertEquals(statement.getStatementNum(),postResponse.getBody().getStatementNum());
 
-
     }
 
     @Test
-    void b_read() {
+    public void b_read() {
         String url = baseURL + "read/"+ statement.getStatementNum();
         System.out.println("URL: " + url);
         ResponseEntity<Statement> response = restTemplate.getForEntity(url,Statement.class);
+        statement = response.getBody();
         assertEquals(statement.getStatementNum(),response.getBody().getStatementNum());
-
-
     }
 
     @Test
-    void c_update() {
+    public void c_update() {
         Statement updated = new Statement.Builder().copy(statement).setDate("14/02/2020").setPaymentDue(67530.00).build();
         String url = baseURL + "update/";
         System.out.println("Post data:" + updated);
         ResponseEntity<Statement> response = restTemplate.postForEntity(url,updated, Statement.class);
-        System.out.println(response);
+        statement = response.getBody();
+        System.out.println(statement);
         assertEquals(statement.getStatementNum(),response.getBody().getStatementNum());
 
-
     }
 
-    @Test
-    void e_delete() {
-        String url = baseURL + "delete/" + statement.getStatementNum();
-        System.out.println("URL: " + url);
-        restTemplate.delete(url);
-    }
 
     @Test
-    void d_getAll() {
+    public void d_getAll() {
         String url = baseURL + "all/";
         HttpHeaders headers= new HttpHeaders();
         HttpEntity<String> entity = new HttpEntity<>(null,headers);
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
         System.out.println(response);
         System.out.println(response.getBody());
-
     }
 
+    @Test
+    public void e_delete() {
+        String url = baseURL + "delete/" + statement.getStatementNum();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
 
+    }
 }
