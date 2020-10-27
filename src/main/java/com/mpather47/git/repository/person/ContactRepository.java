fa88af3895c81a72ef591ec84ed26e6a261423f8
mpather47 <mpather47@gmail.com>
2020-10-28 00:11:03 +0200

    jpa implementation

--- src/main/java/com/mpather47/git/repository/person/ContactRepository.java
+++ src/main/java/com/mpather47/git/repository/person/ContactRepository.java
@@ -3,9 +3,11 @@ package com.mpather47.git.repository.person;
 import com.mpather47.git.entity.person.Contact;
 import com.mpather47.git.entity.person.Person;
 import com.mpather47.git.repository.IRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
+@Repository
+public interface ContactRepository extends JpaRepository<Contact,String >{
 
-public interface ContactRepository extends IRepository<Contact,String> {
-    Set<Contact> getAll();
 }
--- src/main/java/com/mpather47/git/services/person/impl/AddressServiceImpl.java
+++ src/main/java/com/mpather47/git/services/person/impl/AddressServiceImpl.java
@@ -1,54 +1,50 @@
 package com.mpather47.git.services.person.impl;
 
 import com.mpather47.git.entity.person.Address;
-import com.mpather47.git.entity.person.Person;
 import com.mpather47.git.repository.person.AddressRepository;
-import com.mpather47.git.repository.person.PersonRepository;
-import com.mpather47.git.repository.person.impl.AddressRepositoryImpl;
-import com.mpather47.git.repository.person.impl.PersonRepositoryImpl;
 import com.mpather47.git.services.person.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class AddressServiceImpl implements AddressService {
 
-    private static AddressService service=null;
+    @Autowired
     private AddressRepository repository;
-
-    private AddressServiceImpl() {
-        this.repository = AddressRepositoryImpl.getRepository();
-    }
-
-    public static AddressService getService(){
-        if(service==null) service = new AddressServiceImpl();
-        return service;
-    }
-
 
     @Override
     public Set getAll() {
-        return this.repository.getAll();
+        return this.repository.findAll().stream().collect(Collectors.toSet());
     }
 
     @Override
     public Address create(Address address) {
-        return this.repository.create(address);
+        return this.repository.save(address);
     }
 
     @Override
     public Address read(String s) {
-        return this.repository.read(s);
+        return this.repository.findById(s).orElseGet(null);
     }
 
     @Override
     public Address update(Address address) {
-        return this.repository.update(address);
+        if(this.repository.existsById(address.getAddressId())){
+            return this.repository.save(address);
+        }
+        return null;
     }
 
     @Override
     public boolean delete(String s) {
-        return this.repository.delete(s);
+
+         this.repository.deleteById(s);
+        if(this.repository.existsById(s)){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
