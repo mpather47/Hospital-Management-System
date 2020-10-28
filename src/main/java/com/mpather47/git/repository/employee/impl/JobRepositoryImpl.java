e5d254e24c9b0197e7396bde4608c8c51fc3a8fc
PhilisaGqokoma <philisaanita@gmail.com>
2020-10-28 04:55:29 +0200

    Db

--- src/main/java/com/mpather47/git/repository/employee/impl/JobRepositoryImpl.java
+++ src/main/java/com/mpather47/git/repository/employee/impl/JobRepositoryImpl.java
@@ -1,4 +1,4 @@
-package com.mpather47.git.repository.employee.impl;
+/*package com.mpather47.git.repository.employee.impl;
 
 
 import com.mpather47.git.entity.employee.Job;
@@ -66,3 +66,5 @@ public class JobRepositoryImpl implements JobRepository {
         return false;
     }
 }
+
+ */
