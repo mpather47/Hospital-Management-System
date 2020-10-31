f12d4f5fc230821f651ab28752a075cf92735f87
CHRIST003 <christnganga06@gmail.com>
2020-10-31 21:31:54 +0200

    the last

--- src/main/java/com/mpather47/git/HospitalManagementSystem.java
+++ src/main/java/com/mpather47/git/HospitalManagementSystem.java
@@ -3,6 +3,8 @@ package com.mpather47.git;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Alpha last pull request as ask by the lead
+
 @SpringBootApplication
 public class HospitalManagementSystem {
 
