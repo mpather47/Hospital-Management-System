a0e809a7606f845588450eb07a72c9c9bc176db1
XodusDR <sadokun113@gmail.com>
2020-11-01 21:41:12 +0200

    Updating services and controllers redo

--- src/main/java/com/mpather47/git/controller/visit/MedicationController.java
+++ src/main/java/com/mpather47/git/controller/visit/MedicationController.java
@@ -1,42 +1,41 @@
 package com.mpather47.git.controller.visit;
 
-import java.util.Set;
-
-
 import com.mpather47.git.entity.visit.Medication;
 import com.mpather47.git.factory.visit.MedicationFactory;
 import com.mpather47.git.services.visit.Impl.MedicationServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/medication")
 public class MedicationController {
-    @Autowired
     private MedicationServiceImpl medicationService;
 
     @PostMapping("/create")
-    public Medication createMedication(@RequestBody Medication medication){
+    public Medication create (@RequestBody Medication medication) {
         boolean medicationExist = false;
-        Medication newMedication = MedicationFactory.createMedication(medication.getMedicationId(),medication.getVisitId(),medication.getPrescriptionId());
-        if(newMedication !=null){
+        Medication newMedication = MedicationFactory.createMedication(medication.getMedicationId(), medication.getPrescriptionId(), medication.getVisitId());
+        if(newMedication != null){
             medicationExist = true;
 
         }
 
         if (medicationExist){
             return medicationService.create(newMedication);
-
         }
         else return MedicationFactory.createMedication("","","");
+
     }
+
     @GetMapping("/read/{id}")
     public Medication read(@PathVariable String id){
         boolean medicationExist = false;
-        if(id !=null){
+        if (id != null){
             medicationExist = true;
         }
-        if(medicationExist = true){
+        if (medicationExist = true){
             return medicationService.read(id);
 
         }
@@ -44,34 +43,37 @@ public class MedicationController {
     }
 
     @PostMapping("/update")
-    public Medication update(@RequestBody Medication medication){
+    public Medication update (@RequestBody Medication medication){
         boolean medicationExist = false;
-        if(medication != null){
+        if (medication !=null){
             medicationExist = true;
 
+
         }
-        if(medicationExist){
+        if (medicationExist){
             return medicationService.update(medication);
 
         }
         else return null;
-    }
-    @GetMapping("/all")
-    public Set<Medication> getAll(){
-        return medicationService.getAll();
+
     }
 
+    @GetMapping("/all")
+    public Set<Medication> getAll() {return medicationService.getAll();}
+
     @DeleteMapping("/delete/{id}")
-    public boolean delete(@PathVariable String id){
+    public boolean delete (@PathVariable String id){
         boolean medicationExist = false;
         if(id != null){
             medicationExist = true;
-        }
 
+        }
         if(medicationExist){
             return medicationService.delete(id);
 
         }
         else return false;
+
     }
+
 }
--- src/main/java/com/mpather47/git/repository/visit/MedicationRepository.java
+++ src/main/java/com/mpather47/git/repository/visit/MedicationRepository.java
@@ -2,8 +2,10 @@ package com.mpather47.git.repository.visit;
 
 import com.mpather47.git.entity.visit.Medication;
 import com.mpather47.git.repository.IRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Set;
 
-public interface MedicationRepository extends IRepository<Medication, String>{
-    Set<Medication> getAll();
+public interface MedicationRepository extends JpaRepository<Medication, String> {
+
 }
